aa3fea5ebfc2930d48ee849fda04bc33336f2e46
Lukyianenko <110479686+Lukyianenko@users.noreply.github.com>
2022-12-07 18:43:01 +0200

    add Gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,20 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector('.gallery');
+const galleryMurkup = createImageGalleryMurkup(galleryItems);
+
+function createImageGalleryMurkup(galleryItems) {
+    return galleryItems
+    .map(({preview, original, description}) => {
+        return `<a class="gallery__item" href="${original}">
+          <img 
+          class="gallery__image"   
+          src="${preview}"
+          alt="${description}">
+        </a>`
+    }).join('');
+}
+
+galleryBox.insertAdjacentHTML('beforeend', galleryMurkup);
